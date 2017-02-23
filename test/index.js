1ff97ada046c1fb2b4a31f349cb5330a7c0b2847
Andrew Stephan <thekiteeatingtree@gmail.com>
2017-02-22 21:08:22 -0500

    switched to mocha for tests

---
+++ test/index.js
@@ -0,0 +1,57 @@
+'use strict';
+
+const expect = require('code').expect;
+const fs = require('fs');
+const path = require('path');
+
+const pm = require('../index');
+
+describe('xenpm-utils', () => {
+    it('can install and uninstall a node package', function() {
+        this.timeout(10000);
+        const pkg = 'xenon-css-mode';
+        const dir = path.join(__dirname, '..', 'node_modules', pkg);
+
+        return pm.install(pkg, {dir: __dirname}).then(() => {
+            expect(fs.existsSync(path.join(dir, 'package.json'))).to.be.true();
+            return pm.uninstall(pkg, {dir: __dirname});
+        }).then(() => {
+            return expect(fs.existsSync(path.join(dir, 'package.json'))).to.be.false();
+        });
+    });
+
+    it('can install and uninstall multiple node packages', function() {
+        this.timeout(10000);
+        const packages = ['xenon-css-mode', 'left-pad'];
+        const dir = path.join(__dirname, '..', 'node_modules');
+
+        return pm.install(packages, {dir: __dirname}).then(() => {
+            expect(fs.existsSync(path.join(dir, 'xenon-css-mode', 'package.json'))).to.be.true();
+            expect(fs.existsSync(path.join(dir, 'left-pad', 'package.json'))).to.be.true();
+            return pm.uninstall(packages, {dir: __dirname});
+        }).then(() => {
+            expect(fs.existsSync(path.join(dir, 'xenon-css-mode', 'package.json'))).to.be.false();
+            return expect(fs.existsSync(path.join(dir, 'left-pad', 'package.json'))).to.be.false();
+        });
+    });
+
+    it('can search by keyword', function() {
+        return pm.searchByKeyword('xenon').then(results => {
+            const cssMode = results.filter(result => result.name === 'xenon-css-mode');
+            return expect(cssMode.length).to.equal(1);
+        });
+    });
+
+    it('can get outdated info', function() {
+        this.timeout(10000);
+        return pm.outdated('npm', {dir: __dirname}).then(results => {
+            return expect(results.npm).not.to.be.undefined();
+        });
+    });
+
+    it('can view package info', function() {
+        return pm.view('json5').then(results => {
+            return expect(results.name).not.to.be.undefined();
+        });
+    });
+});
